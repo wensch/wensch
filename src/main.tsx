51fb4e0f7315f64e7e21ae4e2ecc9451b3690a77
Mateus Wensch <wensch22@gmail.com>
2025-02-04 12:35:56 -0300

    Insert Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { Analytics } from "@vercel/analytics/react"
 import App from './App.tsx';
 import './i18n';
 import './index.css';
@@ -7,5 +8,6 @@ import './index.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>
 );
